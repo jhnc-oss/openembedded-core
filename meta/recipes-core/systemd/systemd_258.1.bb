jhnc-oss/openembedded-core

commit e9242749621040accba8252d50c036b3e4b10e09

systemd: package 'ukify' seperately
The upgrade to 258.x started shipping 'ukify' by default, but didn't
adjust the runtime dependencies. Add the python modules 'ukify' imports
and split out the package to avoid dragging in gigabytes of python just
plain systemd.

Succesfully tested at runtime on:
* RB1-core-kit, using u-boot EFI mode + systemd-boot
* RB3gen2-core-kit, using Qualcomm UEFI + systemd-boot
* Rubikpi3, using Qualcomm UEFI and type 2 boot
* Beagleboneblack using u-boot UFI mode + systemd-boot

Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>
Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-core/systemd/systemd_258.1.bb
@@ -408,6 +408,7 @@ PACKAGE_BEFORE_PN += "\
     ${PN}-networkd \
     ${PN}-rpm-macros \
     ${PN}-udev-rules \
+    ${PN}-ukify \
     ${PN}-vconsole-setup \
     ${PN}-zsh-completion \
     libsystemd-shared \
@@ -629,6 +630,24 @@ FILES:${PN}-udev-rules = "\
                         ${nonarch_libdir}/udev/rules.d/73-seat-late.rules \
                         ${nonarch_libdir}/udev/rules.d/99-systemd.rules \
 "
+
+SUMMARY:${PN}-ukify = "Create and inspect Unified Kernel Images (UKIs)"
+DESCRIPTION:${PN}-ukify = "ukify is a tool whose primary purpose is to combine components (usually a kernel, an initrd, and a UEFI boot stub) to create a Unified Kernel Image (UKI) — a PE binary that can be executed by the firmware to start the embedded linux kernel."
+FILES:${PN}-ukify = "${bindir}/ukify"
+RDEPENDS:${PN}-ukify = " \
+                        python3-compression \
+                        python3-core \
+                        python3-crypt \
+                        python3-datetime \
+                        python3-io \
+                        python3-json \
+                        python3-netclient \
+                        python3-pefile \
+                        python3-pprint \
+                        python3-pydoc \
+                        python3-pyzstd \
+                        python3-shell \
+                       "
 
 CONFFILES:${PN} = "${sysconfdir}/systemd/coredump.conf \
 	${sysconfdir}/systemd/journald.conf \

commit 2173e3560ccf03c72181e2d7fbe30e8e9e9e923a

systemd: fix excess systemd -> ukify -> python dependency
The systemd package contains a symlink to ukify binary under the
libdir/systemd. This symlink forces the systemd -> systemd-ukify
dependency, which also pulls in a set of python packages, which might be
undesirable for the small isntallations. Move the symlink to the
systemd-ukify package, removing this dependency and preventing the
excess growth of the rootfs / initramfst.

Fixes: e92427496210 ("systemd: package 'ukify' seperately")
Cc: Koen Kooi <koen.kooi@oss.qualcomm.com>
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
Acked-by: Martin Jansa <martin.jansa@gmail.com>
Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>

meta/recipes-core/systemd/systemd_258.1.bb
@@ -633,7 +633,10 @@ FILES:${PN}-udev-rules = "\
 
 SUMMARY:${PN}-ukify = "Create and inspect Unified Kernel Images (UKIs)"
 DESCRIPTION:${PN}-ukify = "ukify is a tool whose primary purpose is to combine components (usually a kernel, an initrd, and a UEFI boot stub) to create a Unified Kernel Image (UKI) — a PE binary that can be executed by the firmware to start the embedded linux kernel."
-FILES:${PN}-ukify = "${bindir}/ukify"
+FILES:${PN}-ukify = " \
+    ${bindir}/ukify \
+    ${nonarch_libdir}/systemd/ukify \
+"
 RDEPENDS:${PN}-ukify = " \
                         python3-compression \
                         python3-core \